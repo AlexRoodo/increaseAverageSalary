2d2460c645cf920826c1de50a517a151458611cc
Alex Roodo <alexroodo6@gmail.com>
2018-01-23 20:10:27 +0300

    Added reading from excel

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String sourceFilePath = "C:\\Users\\aturchenkov\\sourceFile.xlsx";
+        SourceExcelSheet sourceExcelSheet = new SourceExcelSheet();
+        sourceExcelSheet.readFromExcel(sourceFilePath);
     }
 }
--- src/main/java/SourceExcelSheet.java
+++ src/main/java/SourceExcelSheet.java
@@ -1,2 +1,38 @@
-public class SourceExcelSheet {
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
+
+import java.io.*;
+import java.util.Iterator;
+
+class SourceExcelSheet {
+
+    static void readFromExcel(String file) throws IOException{
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+        XSSFSheet sheet = workbook.getSheetAt(0);
+
+        Iterator<Row> rowIterator = sheet.iterator();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+
+            Iterator<Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                switch (cell.getCellTypeEnum()) {
+                    case STRING:
+                        System.out.print(cell.getStringCellValue() + "\t");
+                        break;
+                    case NUMERIC:
+                        System.out.print(cell.getNumericCellValue() + "\t");
+                        break;
+                    case FORMULA:
+                        System.out.print(cell.getNumericCellValue() + "\t");
+                        break;
+                    default :
+                }
+            }
+
+            System.out.println();
+        }
+    }
 }
