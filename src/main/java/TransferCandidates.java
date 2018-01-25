0d6936c83eade0ce74dfb1ce51dcac5b487c1884
aturchenkov <aturchenkov@tsconsulting.ru>
2018-01-25 15:31:30 +0300

    Collections now are stored in TransferCandidate class and for departments storage is used HashMap collection

--- src/main/java/TransferCandidates.java
+++ src/main/java/TransferCandidates.java
@@ -1,36 +1,47 @@
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 class TransferCandidates {
     private LinkedList<Employee> transferCandidatesList = new LinkedList<>();
+    private HashMap<String, Department> departmentHashMap = null;
     LinkedList<Employee> resultTransferCandidatesList = new LinkedList<>();
 
-    void searchForCandidate (SourceExcelSheet sourceExcelSheet) {
-        for (Department dprt : sourceExcelSheet.departments) {
-            for (Employee empl : dprt.getEmployeesList()) {
-                if(dprt.getAverageSalary()
-                        .compareTo(dprt.getTotalSalary()
-                                .subtract(empl.getSalary()
-                                        .divide(new BigDecimal(dprt.getEmployeesList().size() - 1), RoundingMode.HALF_UP))) < 0) {
-                    transferCandidatesList.add(empl);
+    void searchForCandidate () {
+        for (String s : departmentHashMap.keySet()) {
+            Department dpt = departmentHashMap.get(s);
+            for (Employee emp : dpt.getEmployeesList()) {
+                if(dpt.getAverageSalary()
+                        .compareTo(dpt.getTotalSalary()
+                                .subtract(emp.getSalary())
+                                .divide(new BigDecimal(dpt.getEmployeesList().size() - 1), RoundingMode.HALF_UP)) < 0) {
+                    transferCandidatesList.add(emp);
                 }
             }
         }
     }
 
-    void filterCandidate (SourceExcelSheet sourceExcelSheet) {
+    void filterCandidate () {
         for (Employee empl : transferCandidatesList) {
-            for (Department dprt : sourceExcelSheet.departments) {
-                if (dprt.getAverageSalary()
-                        .compareTo((dprt.getTotalSalary()
+            for (String s : departmentHashMap.keySet()) {
+                Department dpt = departmentHashMap.get(s);
+                if (dpt.getAverageSalary()
+                        .compareTo((dpt.getTotalSalary()
                                 .add(empl.getSalary()))
-                                .divide(new BigDecimal(dprt.getEmployeesList().size() + 1), RoundingMode.HALF_UP)) < 0) {
+                                .divide(new BigDecimal(dpt.getEmployeesList().size() + 1), RoundingMode.HALF_UP)) < 0) {
                     System.out.println("Добавление сотрудника " + empl.getName());
-                    empl.targetDepartments.append(dprt.getDepartmentName()).append("\n");
                     resultTransferCandidatesList.add(empl);
                 }
             }
         }
     }
+
+    public HashMap<String, Department> getDepartmentHashMap() {
+        if (departmentHashMap == null) {
+            departmentHashMap = new HashMap<>();
+        }
+        return departmentHashMap;
+    }
+
 }
