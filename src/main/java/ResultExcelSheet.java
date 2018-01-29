0808007faf7c3edfaa50146d2ea50d141999fba4
aturchenkov <aturchenkov@tsconsulting.ru>
2018-01-29 17:36:31 +0300

    Added excel header

--- src/main/java/ResultExcelSheet.java
+++ src/main/java/ResultExcelSheet.java
@@ -4,6 +4,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 class ResultExcelSheet {
@@ -15,16 +16,37 @@ class ResultExcelSheet {
         try(Workbook book = new XSSFWorkbook()) {
             Sheet sheet = book.createSheet("Transfer");
 
+            Iterator<Transfer> iterator = transferLinkedList.listIterator();
+            String currentDepartmentName = transferLinkedList.getFirst().getCurrentDepartment()
+                    .getName();
+
+
+            while (iterator.hasNext()) {
+                Transfer currentTransfer = iterator.next();
+                if (!currentTransfer.getCurrentDepartment().getName()
+                        .equalsIgnoreCase(currentDepartmentName)) {
+                    Row row = sheet.createRow(i);
+                    Cell departmentName = row.createCell(0);
+                    departmentName.setCellValue(currentTransfer.getCurrentDepartment().getName());
+                    Cell averageSalaryValue = row.createCell(1);
+                    averageSalaryValue.setCellValue(currentTransfer.getCurrentDepartment()
+                            .getAverageSalary().doubleValue());
+                }
+            }
+
             for (Transfer transfer : transferLinkedList) {
                 Row row = sheet.createRow(i);
                 Cell name = row.createCell(0);
-                name.setCellValue(currentEmployee.getName());
+                //name.setCellValue(currentEmployee.getName());
                 Cell salary = row.createCell(1);
-                salary.setCellValue(currentEmployee.getSalary().toString());
+                //salary.setCellValue(currentEmployee.getSalary().toString());
                 i++;
             }
 
             sheet.autoSizeColumn(0);
+            sheet.autoSizeColumn(1);
+            sheet.autoSizeColumn(2);
+
 
             book.write(new FileOutputStream(file));
         } catch (FileNotFoundException e) {
